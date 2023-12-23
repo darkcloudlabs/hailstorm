9fb8a670b07bc6a949217afeb50bc93f829270dc
anthdm <cryptoanthdm@gmail.com>
2023-12-23 09:57:38 +0100

    fixed blob return in json

--- pkg/types/deploy.go
+++ pkg/types/deploy.go
@@ -10,6 +10,6 @@ type Deploy struct {
 	ID        uuid.UUID `json:"id"`
 	AppID     uuid.UUID `json:"appId"`
 	Region    string    `json:"region"`
-	Blob      []byte
+	Blob      []byte    `json:"-"`
 	CreatedAT time.Time `json:"createdAt"`
 }
